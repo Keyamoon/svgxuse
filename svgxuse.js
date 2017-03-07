5df99a4a0a00a265d540a1e71081d0ace21f8bea
Keyamoon <keyamoon@gmail.com>
2017-03-07 02:43:06 -0800

    Update commented versions to 1.2.3

--- svgxuse.js
+++ svgxuse.js
@@ -2,7 +2,7 @@
  * @copyright Copyright (c) 2017 IcoMoon.io
  * @license   Licensed under MIT license
  *            See https://github.com/Keyamoon/svgxuse
- * @version   1.2.2
+ * @version   1.2.3
  */
 /*jslint browser: true */
 /*global XDomainRequest, MutationObserver, window */
